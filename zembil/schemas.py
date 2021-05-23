a77361a1d9dadb92e88f80cbcafe0f6354c42a3c
Surafel Kassahun <se.surafel.kassahun@gmail.com>
2021-05-23 19:23:13 +0300

    Additional Schemas necessary for endpoints added

--- zembil/schemas.py
+++ zembil/schemas.py
@@ -40,12 +40,38 @@ class ProductSchema(ma.Schema):
     brand = ma.Nested(BrandSchema)
     category = ma.Nested(CategorySchema)
 
+class ShopProductSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "user", "building_name", "phone_number1", 
+        "phone_number2", "category", "location", "description", "products")
+        model = ShopModel
+    user = ma.Nested(UserSchema)
+    category = ma.Nested(CategorySchema)
+    location = ma.Nested(LocationSchema)
+    products = ma.Nested(ProductSchema)
+
 class ReviewSchema(ma.Schema):
     class Meta:
         fields = ("id", "user", "product", "rating", "user_review", "date")
         model = ReviewModel
     user = ma.Nested(UserSchema)
     product = ma.Nested(ProductSchema)
+
+class ProductReviewSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "brand", "name", "date", "building_name", 
+        "description", "category", "price", "condition", "image", 
+        "delivery_available", "discount", "product_count", "reviews")
+        model = ProductModel
+    brand = ma.Nested(BrandSchema)
+    category = ma.Nested(CategorySchema)
+    reviews = ma.Nested(ReviewSchema)
+
+class CategoryShopsSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "name", "shops")
+        model = CategoryModel
+    shops = ma.Nested(ShopSchema)
 
 class WishListSchema(ma.Schema):
     class Meta:
@@ -54,6 +80,12 @@ class WishListSchema(ma.Schema):
     product = ma.Nested(ProductSchema)
     user = ma.Nested(UserSchema)
 
+class UserWishListSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "username", "email", "role", "phone", "wishlists")
+        model = UserModel
+    wishlists = ma.Nested(WishListSchema)
+
 class ShopLikeSchema(ma.Schema):
     class Meta:
         fields = ("id", "user", "shop")
@@ -61,4 +93,11 @@ class ShopLikeSchema(ma.Schema):
     user = ma.Nested(UserSchema)
     shop = ma.Nested(ShopSchema)
 
+class TotalShopLikes(ma.Schema):
+    class Meta:
+        fields = ("id", "user", "shoplikes", "building_name", "phone_number1", 
+        "phone_number2", "category", "location", "description")
+        model = ShopLikeModel
+    user = ma.Nested(UserSchema)
+    shoplikes = ma.Nested(ShopLikeSchema)
 
